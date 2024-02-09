99e9e7e2f57433b87cc072ce71adce1eeb833b4f
aldiaaaaan <aldian.asmr@gmail.com>
2024-02-09 17:04:06 +0700

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 import svgLoader from 'vite-svg-loader'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), svgLoader()],
-  build: {
-    outDir: `./dist`
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue(), svgLoader()],
+    build: {
+      assetsDir: `${env.VITE_WEB_CLIENT_STATIC_PREFIX}`
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
